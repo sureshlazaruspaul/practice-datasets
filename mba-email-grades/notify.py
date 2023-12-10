0f5cb45b0f1ebb0c26abf13b76d5d959edf445d9
Suresh Paul <sureshlazaruspaul@gmail.com>
2023-12-09 23:11:43 -0500

    added some files

--- mba-email-grades/notify.py
+++ mba-email-grades/notify.py
@@ -13,7 +13,7 @@ message = f"Hello All,\n\n"
 message += f"The final grade for our course has been posted.\n"
 message += f"Have a nice break. Happy Holidays!\n\n"
 message += f"PS: email sent for Prof. Suresh Paul, by Python Assistant.\n\n"
-message += f"logfile of the email program available at,\n{logfile}"
+message += f"logfile of the email program available at,\n{logfile}\n"
 
 notify_by_email(contact_list,subject,message)
 print(message)
