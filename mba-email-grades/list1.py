c75856e725918f38199b7cb8ba0d14d212a20678
Suresh Paul <sureshlazaruspaul@gmail.com>
2023-12-09 23:02:39 -0500

    added some files

--- mba-email-grades/list1.py
+++
@@ -1,5 +0,0 @@
-def contact_list():
-    return {
-        "Suresh Paul": ("+1234567890","spaul@bw.edu")
-    }
-
--- mba-email-grades/notify.py
+++ mba-email-grades/notify.py
@@ -1,6 +1,6 @@
 # libraries & dependencies
 from send_email import notify_by_email
-from list1 import contact_list
+from list import contact_list
 
 contact_list=contact_list()
 print(contact_list)
@@ -14,3 +14,6 @@ message += f"PS: email sent for Prof. Suresh Paul, by Python Assistant.\n"
 print(message)
 
 notify_by_email(contact_list,subject,message)
+
+logfile = "https://github.com/sureshlazaruspaul/practice-datasets/blob/main/mba-email-grades/notify.logfile"
+print(f"logfile of the email program availbale at,\n{logfile}")
